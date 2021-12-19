a8b70d9faf4081da70c79aab0d21b0b0a73c9cdf
FilipeMSouza <filipe.souza1906@gmail.com>
2021-12-18 23:06:15 -0300

    Setting some configurations
    - Intalled packs
      - scss
      - typescript
    
    - Cofiguring some files inside src/pages
    
      - _documents prymari cofiguration as fonts
    
    - Adding global styles, you can fint it in styles/global.scss
    
    - Setting colors to correspond to project layout
      - [figma link]
        (https://www.figma.com/file/zqToEpGuJBBBy2Ulu18lPK/ig.news-(Copy)?node-id=1%3A2)
    - Setting font size to adapt to difernt screens,
    - Setting project font, cursor to button, link style

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+
+import Head from 'next/head';
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ig.News | Welcome</title>
+      </Head>
+      <h1>
+        Hello, Ig.News
+      </h1>
+    </>
+  )
+}
